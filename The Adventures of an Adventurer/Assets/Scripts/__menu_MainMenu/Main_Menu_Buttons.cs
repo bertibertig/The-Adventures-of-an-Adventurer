74dbfab941adefe3aa76731559cc79158813f2a1
Cedric Ferstl <Cedric Ferstl>
2016-06-07 09:13:21 +0200

    main menu turns off health bar

--- The Adventures of an Adventurer/Assets/Scripts/__menu_MainMenu/Main_Menu_Buttons.cs
+++ The Adventures of an Adventurer/Assets/Scripts/__menu_MainMenu/Main_Menu_Buttons.cs
@@ -8,11 +8,17 @@ public class Main_Menu_Buttons : MonoBehaviour {
     private GameObject tempMusic;
     private AudioSource Menu_Music;
     private static bool Music_is_playing;
+	private GameObject[] UIElements;
 
     void Start()
     {
+		UIElements = GameObject.FindGameObjectsWithTag("UI");
         tempMusic = GameObject.FindGameObjectWithTag("Music");
         Menu_Music = tempMusic.GetComponent<AudioSource>();
+		if (UIElements != null) {
+			foreach (GameObject go in UIElements)
+				go.SetActive (false);
+		}
         if (!Music_is_playing)
         {
             Menu_Music.Play();
@@ -25,6 +31,8 @@ public class Main_Menu_Buttons : MonoBehaviour {
 	{
         Menu_Music.Stop();
         Destroy(Menu_Music);
+		foreach (GameObject go in UIElements)
+			go.SetActive (true);
 		Application.LoadLevel(levelToLoad);
 	}
 
