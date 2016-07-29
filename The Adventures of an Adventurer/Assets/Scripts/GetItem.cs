152683b026b1a0dc209f90b72b8c18b6fc9c57d8
Sascha Bertleff <sascha.bertleff@gmail.com>
2016-07-29 20:06:14 +0200

    no message

---
+++ The Adventures of an Adventurer/Assets/Scripts/GetItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GetItem : MonoBehaviour {
+
+    public Text multiplaier;
+    public Image item;
+
+    private List<Inventory_Database.Item> itemList;
+
+    void Start()
+    {
+        multiplaier.enabled = false;
+        item.enabled = false;
+    }
+
+    public void ShowGetItem(int itemID, int number)
+    {
+        itemList = GameObject.FindGameObjectWithTag("InventoryUI").GetComponent<Inventory_Database>().GetItemDatabase;
+        item.sprite = itemList[itemID].GetSprite;
+        multiplaier.text = "x" + number.ToString();
+        StartCoroutine("ShowItem");
+    }
+
+    IEnumerator ShowItem()
+    {
+        multiplaier.enabled = true;
+        item.enabled = true;
+        yield return new WaitForSeconds(5);
+        multiplaier.enabled = false;
+        item.enabled = false;
+    }
+}
