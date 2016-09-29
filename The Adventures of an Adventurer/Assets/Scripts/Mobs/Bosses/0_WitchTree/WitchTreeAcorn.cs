a0e613bfba02381a311e018db122be5f0cfe1e4c
Sascha Bertleff <sascha.bertleff@gmail.com>
2016-09-29 22:29:00 +0200

    Squirel shoots Acorns, but in in Random directions (player tracking needs to be implementet)

--- The Adventures of an Adventurer/Assets/Scripts/Mobs/Bosses/0_WitchTree/WitchTreeAcorn.cs
+++ The Adventures of an Adventurer/Assets/Scripts/Mobs/Bosses/0_WitchTree/WitchTreeAcorn.cs
@@ -3,15 +3,40 @@ using System.Collections;
 
 public class WitchTreeAcorn : MonoBehaviour {
 
-    public GameObject witchTreeLeaveCollider;
+    private GameObject[] witchTreeLeaveColliders;
+    private Damage_On_Collision damageDealer;
+    private bool onGround;
 
 	// Use this for initialization
 	void Start () {
-        Physics2D.IgnoreCollision(witchTreeLeaveCollider.GetComponent<EdgeCollider2D>(), GetComponent<PolygonCollider2D>());
+        witchTreeLeaveColliders = GameObject.FindGameObjectsWithTag("LeaveCollider");
+        foreach(GameObject g in witchTreeLeaveColliders)
+            Physics2D.IgnoreCollision(g.GetComponent<EdgeCollider2D>(), GetComponent<PolygonCollider2D>());
+        damageDealer = gameObject.GetComponentInChildren<Damage_On_Collision>();
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Enemy"))
+            Physics2D.IgnoreCollision(g.GetComponent<PolygonCollider2D>(), GetComponent<PolygonCollider2D>());
+        onGround = false;
+        StartCoroutine("Disapear");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(0, 0, -(this.gameObject.transform.rotation.z + 3)));
-	}
+        if(!onGround)
+            transform.Rotate(new Vector3(0, 0, -(this.gameObject.transform.rotation.z + 5)));
+        if (onGround)
+            Destroy(damageDealer);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Ground") || col.collider.CompareTag("Projectile"))
+            onGround = true;
+    }
+
+    IEnumerator Disapear()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(this.gameObject);
+    }
 }
--- The Adventures of an Adventurer/Assets/Scripts/Mobs/Bosses/0_WitchTree/WitchTreeSquirel.cs
+++ The Adventures of an Adventurer/Assets/Scripts/Mobs/Bosses/0_WitchTree/WitchTreeSquirel.cs
@@ -3,15 +3,30 @@ using System.Collections;
 
 public class WitchTreeSquirel : MonoBehaviour {
 
+    private GameObject acorn;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
+        acorn = Resources.Load("Mobs/Bosses/0_WitchTree/Acorn", typeof(GameObject)) as GameObject;
+        anim = this.gameObject.GetComponent<Animator>();
+        anim.SetBool("throw", false);
         StartCoroutine("ThrowNuts");
 	}
 
     private IEnumerator ThrowNuts()
     {
-
-        yield return null;
+        while (true)
+        {
+            anim.SetBool("throw", true);
+            yield return new WaitForSeconds(0.3f);
+            Vector3 currentPosition = new Vector3(this.gameObject.transform.position.x + 0.1f, this.gameObject.transform.position.y + 0.1f, this.gameObject.transform.position.z);
+            GameObject tmpObj = GameObject.Instantiate(acorn, currentPosition, Quaternion.identity) as GameObject;
+            tmpObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-70, 200), Random.Range(0, 200)));  
+            yield return new WaitForSeconds(1);
+            anim.SetBool("throw", false);
+            yield return new WaitForSeconds(2);
+            
+        }
     }
 }
