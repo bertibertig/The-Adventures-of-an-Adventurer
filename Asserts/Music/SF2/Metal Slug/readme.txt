5c10893779e73749eaa4306822526cabca91520a
SASCHABERTLEFFL\Sascha Bertleff <sascha.bertleff@gmail.com>
2016-10-28 09:04:30 +0200

    Updated Documents and cleaned Folders

--- Asserts/Music/SF2/Metal Slug/readme.txt
+++
@@ -1,3 +0,0 @@
-You need VOPM installed in order to get the fm basses and lead guitar from the flp file working.
-
--Txai
--- The Adventures of an Adventurer/Assets/Scripts/Mobs/Bosses/0_WitchTree/WitchTreeSquirel.cs
+++ The Adventures of an Adventurer/Assets/Scripts/Mobs/Bosses/0_WitchTree/WitchTreeSquirel.cs
@@ -5,12 +5,17 @@ public class WitchTreeSquirel : MonoBehaviour {
 
     private GameObject acorn;
     private Animator anim;
+    private GameObject player;
+    private float a;
+    private float t;
+    private float g;
 
 	// Use this for initialization
 	void Start () {
         acorn = Resources.Load("Mobs/Bosses/0_WitchTree/Acorn", typeof(GameObject)) as GameObject;
         anim = this.gameObject.GetComponent<Animator>();
         anim.SetBool("throw", false);
+        player = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine("ThrowNuts");
 	}
 
@@ -20,9 +25,15 @@ public class WitchTreeSquirel : MonoBehaviour {
         {
             anim.SetBool("throw", true);
             yield return new WaitForSeconds(0.3f);
+
+            
+            float x = (player.GetComponent<Transform>().transform.position.x);
+            float y = (player.GetComponent<Transform>().transform.position.y);
+            //float v0 = (Mathf.Pow(x,2)*y) * (1/Mathf.Pow(x,2))*(9.81f/2);
+
             Vector3 currentPosition = new Vector3(this.gameObject.transform.position.x + 0.1f, this.gameObject.transform.position.y + 0.1f, this.gameObject.transform.position.z);
             GameObject tmpObj = GameObject.Instantiate(acorn, currentPosition, Quaternion.identity) as GameObject;
-            tmpObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-70, 200), Random.Range(0, 200)));  
+            tmpObj.GetComponent<Rigidbody2D>().AddForce(new Vector2(2 * x, 2* y));  
             yield return new WaitForSeconds(1);
             anim.SetBool("throw", false);
             yield return new WaitForSeconds(2);
