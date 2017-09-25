d25036a3101bc346337ba7fd6fbd1ea5b3466126
Sascha Bertleff <sascha.bertleff@gmail.com>
2017-09-26 00:35:27 +0200

    Repair of Broken Merge

--- 03_UnityProject/The Adventures of an Adventurer/Assets/Scripts/Health_Controller.cs
+++ 03_UnityProject/The Adventures of an Adventurer/Assets/Scripts/Health_Controller.cs
@@ -15,6 +15,7 @@ public class Health_Controller : MonoBehaviour {
     private bool knockbackEnabled;
     private Animator anim;
     private Player_Movement player;
+	private GameObject damageSource;
     private bool isDead = false;
     private bool isInvincible = false;
 
@@ -123,7 +124,7 @@ public class Health_Controller : MonoBehaviour {
     void Damage()
     {
         if (knockbackEnabled)
-            player.StartKnockback(0.2f, -5f, player.transform.position);
+			player.StartKnockback(250f, player.transform.position, damageSource.transform.position);
         UpdateGUI();
     }
 
@@ -138,5 +139,8 @@ public class Health_Controller : MonoBehaviour {
         healthGUI.fillAmount = health / maxHealth;
     }
 
-
+	void SetEnemyPlayerGotHitBy(GameObject enemy)
+	{
+		this.damageSource = enemy;
+	}
 }
