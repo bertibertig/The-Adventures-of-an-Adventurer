ef503479bbd115077353c0c3181c71111a82431d
V5-W10-573G\Cedric <cedric.ferstl@gmail.com>
2017-10-18 09:19:25 +0200

    Partially included second attack of first boss.

---
+++ 03_UnityProject/The Adventures of an Adventurer/Assets/Scripts/Bird_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird_Movement : MonoBehaviour {
+
+	private Move_To_Waypoint mtw;
+
+	// Use this for initialization
+	void Start () {
+		mtw = GetComponent<Move_To_Waypoint>();
+
+		Move ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Move()
+	{
+		mtw.MoveFromAToB ();
+	}
+}
