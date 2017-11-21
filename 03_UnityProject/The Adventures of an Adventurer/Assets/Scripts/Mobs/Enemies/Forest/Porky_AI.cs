f9cf1f4df6b5b919deb514e66a98ebd71aebf18c
DESKTOP-GU43859\sascha <sascha.bertleff@gmail.com>
2017-11-21 14:58:10 +0100

    Added Porky to the Mobs and Created Playerchecker which checks if player is in a certan range.

---
+++ 03_UnityProject/The Adventures of an Adventurer/Assets/Scripts/Mobs/Enemies/Forest/Porky_AI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porky_AI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponentInChildren<PlayerChecker>().PlayerEnter += Porky_AI_PlayerEnter;
+        GetComponentInChildren<PlayerChecker>().PlayerExiting += Porky_AI_PlayerExiting;
+	}
+
+    private void Porky_AI_PlayerExiting(object sender, System.EventArgs e)
+    {
+        print("Following Player");
+    }
+
+    private void Porky_AI_PlayerEnter(object sender, System.EventArgs e)
+    {
+        print("Leave Player");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
