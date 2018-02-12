c1b9b0f50ecea44d399564ef795c00642b059f93
DESKTOP-GU43859\sascha <sascha.bertleff@gmail.com>
2018-02-12 17:51:56 +0100

    Added Villager-Prefabs for Cutscene Added Villager Spawnig Script

---
+++ 03_UnityProject/The Adventures of an Adventurer/Assets/Scripts/NPCs/Villagers/VillagerAI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerAI : MonoBehaviour {
+
+    public bool runningEnabled = true;
+    [Header("Position where the villager should stop Running")]
+    public Vector3 endPos = new Vector2(0,0);
+    [Header("Running Speed")]
+    public float speed = 3;
+    [Header("Fastest Speed the villager runs")]
+    public float maxSpeed = 3;
+
+    private Vector2 dirNormalized;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start ()
+    {
+        dirNormalized = (endPos - transform.position).normalized;
+        rb2d = this.gameObject.GetComponent<Rigidbody2D>();
+        IgnorePlayerColliders();
+        if (runningEnabled)
+        {
+            StartCoroutine("Run");
+        }
+	}
+
+    private IEnumerator Run()
+    {
+        do
+        {
+            if (Vector2.Distance(endPos, transform.position) <= 1)
+                runningEnabled = false;
+            else
+                ControlMaxSpeedX();
+            yield return new WaitForFixedUpdate();
+        } while (runningEnabled);
+    }
+
+    private void ControlMaxSpeedX()
+    {
+        if (rb2d.velocity.x > maxSpeed)
+            rb2d.velocity = new Vector2(maxSpeed,0);
+        if (rb2d.velocity.x < -maxSpeed)
+            rb2d.velocity = new Vector2(-maxSpeed, 0);
+        else
+        {
+            float xSpeed = transform.position.x + dirNormalized.x * speed * Time.deltaTime;
+            rb2d.AddForce(new Vector2(xSpeed, 0));
+        }
+    }
+
+    public void IgnorePlayerColliders()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(this.GetComponent<PolygonCollider2D>(), player.GetComponent<PolygonCollider2D>());
+        Physics2D.IgnoreCollision(this.GetComponent<PolygonCollider2D>(), player.GetComponents<CircleCollider2D>()[0]);
+        Physics2D.IgnoreCollision(this.GetComponent<PolygonCollider2D>(), player.GetComponents<CircleCollider2D>()[1]);
+    }
+}
--- 03_UnityProject/The Adventures of an Adventurer/Assets/Scripts/Sequences/01_Beginning/SpawnRunningVillagers.cs
+++ 03_UnityProject/The Adventures of an Adventurer/Assets/Scripts/Sequences/01_Beginning/SpawnRunningVillagers.cs
@@ -1,11 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnRunningVillagers : MonoBehaviour {
 
-	public static void SpawnVillagers()
-    {
+    [Header ("Ressource Path of the Villagers e.g.: NPCs/Villagers")]
+    public string ressourcePath = "NPCs/Villagers";
 
+    [Header ("X and Y Axis of the Spawn Position for the Villagers")]
+    public Vector2 villagersSpawnPosition = new Vector2(0,0);
+
+    [Header ("0 = Right, 180 = Left")]
+    public float roation = 180;
+
+    [Header("Seconds until a new Villager is spawned")]
+    public float minvillagerDelay = 5;
+    public float maxvillagerDelay = 5;
+
+    public bool SpawnVillagers { get; set; }
+
+    private static GameObject[] villagers;
+    private int lastSpawnedVillagerId = 0;
+
+
+    private void Start()
+    {
+        SpawnVillagers = true;
+        LoadVillagers();
+        StartCoroutine("SpawnVillagersCoRoutine");
+    }
+
+    private void LoadVillagers()
+    {
+        object[] tmpVillagers= Resources.LoadAll(ressourcePath, typeof(GameObject));
+        villagers = Array.ConvertAll(tmpVillagers, i => (GameObject) i);
+    }
+
+    private IEnumerator SpawnVillagersCoRoutine()
+    {
+        do
+        {
+            //Couple Chance
+            if (UnityEngine.Random.Range(0, 10) == 10)
+            {
+                SpawnVillager();
+                yield return new WaitForSeconds(0.3f);
+            }
+            SpawnVillager();
+
+            float villagerDelay = UnityEngine.Random.Range(minvillagerDelay, maxvillagerDelay);
+            yield return new WaitForSeconds(villagerDelay);
+        } while (SpawnVillagers);
+    }
+
+	public void SpawnVillager()
+    {
+        int villagerId = UnityEngine.Random.Range(0, 4);
+        if (villagerId == lastSpawnedVillagerId)
+            if (villagerId - 1 < 0)
+                villagerId++;
+            else
+                villagerId--;
+        Instantiate(villagers[villagerId], new Vector3(villagersSpawnPosition.x, villagersSpawnPosition.y), Quaternion.Euler(0, roation, 0));
+        lastSpawnedVillagerId = villagerId;
     }
 }
